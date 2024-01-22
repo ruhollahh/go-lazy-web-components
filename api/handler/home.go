4432f8f98141bd3c21dd832a6fd9865b33abbce7
Ruhollah Hosseini <ruhollahh01@gmail.com>
2024-01-22 20:37:46 +0330

    fix deprecated warning on rand.Seed

--- api/handler/home.go
+++ api/handler/home.go
@@ -15,8 +15,9 @@ func (h *Handler) Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) ShowRandomLazyComp(w http.ResponseWriter, r *http.Request) {
-	rand.Seed(time.Now().UnixNano())
-	random := rand.Intn(2)
+	source := rand.NewSource(time.Now().UnixNano())
+	rng := rand.New(source)
+	random := rng.Intn(2)
 	if random == 0 {
 		components.LazyCompOne().Render(context.Background(), w)
 	} else {
